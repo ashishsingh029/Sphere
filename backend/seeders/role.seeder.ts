fab1cbaf7f2a1d0a157751926035c0ce9c1ebbe7
Ashish Kumar Singh <162873962+ashishsingh029@users.noreply.github.com>
2025-02-15 10:59:32 +0530

    Readme Seed Instruction added

--- backend/seeders/role.seeder.ts
+++ backend/seeders/role.seeder.ts
@@ -26,6 +26,7 @@ const seedRoles = async () => {
             }
         }
         console.log('Seeding completed successfully.')
+        process.exit(1)
     } catch (error: any) {
         console.error('Error during seeding: ', error)
     }
